2d83f8ebede2bcb1bfaee90260e26141d850741f
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-13 19:51:10 -0700

    WIP add player icon on click

--- main.js
+++ main.js
@@ -13,32 +13,52 @@ var difficult = {
 };
 var game = new Game(human, computer, classic);
 
-game.playRound(game.gameType.choices, 'rock-on');
-game.chooseWinnerDifficult(game.human, game.computer);
-console.log(game.human.name, ': ', game.human.selection, '\n', game.computer.name, ': ', game.computer.selection, '\n', 'Winner : ', game.winner);
+// game.playRound(game.gameType.choices, 'rock-on');
+// game.chooseWinnerDifficult(game.human, game.computer);
+// console.log(game.human.name, ': ', game.human.selection, '\n', game.computer.name, ': ', game.computer.selection, '\n', 'Winner : ', game.winner);
 
 //////
-var rock = document.querySelector('rock-js');
-var paper = document.querySelector('paper-js');
-var scissors = document.querySelector('scissors-js');
-var alien = document.querySelector('alien-js');
-var rockOn = document.querySelector('rock-on-js');
+var rock = document.querySelector('.rock-js');
+var paper = document.querySelector('.paper-js');
+var scissors = document.querySelector('.scissors-js');
+var alien = document.querySelector('.alien-js');
+var rockOn = document.querySelector('.rock-on-js');
+var humanSelectionIcon = document.querySelectorAll('h6');
 
-rock.addEventListener('click', updatePlayerChoice);
-paper.addEventListener('click', updatePlayerChoice);
-scissors.addEventListener('click', updatePlayerChoice);
-alien.addEventListener('click', updatePlayerChoice);
-rockOn.addEventListener('click', updatePlayerChoice);
+rock.addEventListener('click', chooseIcon);
+paper.addEventListener('click', chooseIcon);
+scissors.addEventListener('click', chooseIcon);
+alien.addEventListener('click', chooseIcon);
+rockOn.addEventListener('click', chooseIcon);
 window.addEventListener('load', displayPlayerInfo);
+// humanSelectionIcon.addEventListener('click', toggleIcon)
 
-function updatePlayerChoice() {
+
+function chooseIcon() {
+  updateHumanSelection();
+  showHumanChoice();
+};
+
+function updateHumanSelection() {
   if (event.target.classList.contains('rock-js')) {
     game.human.selection = 'rock';
   } else if (event.target.classList.contains('paper-js')) {
     game.human.selectoin = 'paper';
   } else if (event.target.classList.contains('scissor-js')) {
     game.human.selection = 'scissors';
+  } else if (event.target.classList.contains('alien-js')) {
+    game.human.selection = 'alien';
+  } else if (event.target.classList.contains('rock-on-js')) {
+    game.human.selection = 'rock-on';
   };
+};
+
+function showHumanChoice() {
+  // event.target.append(`<p>${game.human.token}</p>`);
+  console.log(event.target.offsetTop, event.target.offsetLeft)
+  var humanIcon = document.createElement('div');
+  humanIcon.classList.add('position-offset');
+  humanIcon.innerText = `${game.human.token}`
 }
 
 function displayPlayerInfo() {
@@ -63,3 +83,7 @@ function updatePlayer2Info() {
   player2Name.innerText = game.computer.name;
   player2Wins.innerText = `Wins: ${game.computer.wins}`;
 }
+
+// function toggleIcon() {
+//   event.target.append(`<h6>${game.human.token}</h6}`);
+// }
